b6e1897f444f4bb296b8937bb49d97e0fd6f172c
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-28 15:38:12 +0100

    feat added es vite-vue

---
+++ gio-vue-vite/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-red">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+<script>
+export default{
+  data(){
+    return{
+      message:'La mia prima app con Vite!'
+    }
+  },
+}
+</script>
+<style>
+.bg-red{
+    background-color: blue;
+    color: red;
+}
+</style>
